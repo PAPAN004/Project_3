9ea4fe3f2da7eb91a2c929cee26ac8f84fcf9076
PAPAN004 <155662184+PAPAN004@users.noreply.github.com>
2024-02-08 11:42:07 -0500

    Update module_template.cpp

--- module_template.cpp
+++ module_template.cpp
@@ -7,33 +7,13 @@
 //=====[Declaration of private data types]=====================================
 
 //=====[Declaration and initialization of public global objects]===============
-AnalogIn lightSensor(A1);
-DigitalOut entryLightLed(D1);
+
 //=====[Declaration of external public global variables]=======================
 
 //=====[Declaration and initialization of public global variables]=============
 
 //=====[Declaration and initialization of private global variables]============
 //=====[Declarations (prototypes) of private functions]========================
-void powerToLed()
 //=====[Implementations of public functions]===================================
 
 //=====[Implementations of private functions]==================================
-void PowerToLed()
-{
-    entryLightLed = OFF;
-    char str[20];
-    while (true) {
-        float lightSensorReading = lightSensor.read();
-        sprintf(str,"light Level: %.2f\r\n", lightSensorReading);
-        int stringLength = strlen(str);
-        uartUsb.write(str, stringLength);
-        if (lightSensorReading>0.5) {
-        entryLightLed = ON;
-        }
-        else
-        {
-        entryLightLed = OFF;
-        }
-}
-}
