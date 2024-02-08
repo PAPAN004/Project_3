479112770f801f29ae07a7ec8f681bff01abf910
mhelfri <155662290+mhelfri@users.noreply.github.com>
2024-02-08 12:01:08 -0500

    Update and rename wiper_subsytem.h to wiper.h

---
+++ wiper.h
@@ -0,0 +1,13 @@
+//=====[#include guards - begin]===============================================
+#ifndef _WIPER_H_
+#define _WIPER_H_
+
+//=====[Declaration of public defines]=========================================
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+//=====[#include guards - end]=================================================
+
+#endif
