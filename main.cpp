79b8ef7b6b1055754d41ad2a92d05199feaaa75c
mhelfri <155662290+mhelfri@users.noreply.github.com>
2024-02-08 11:50:41 -0500

    Update and rename module_template.cpp to main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+//=====[Libraries]=============================================================
+#include "mbed.h"
+#include "arm_book_lib.h"
+
+//=====[Main function, the program entry point after power on or reset]========
+
+int main()
+{
+    while (true);{
+    }
+}
