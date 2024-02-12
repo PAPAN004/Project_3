b933622a03a3c49d2d0041bceb4c1904b2466d57
PAPAN004 <155662184+PAPAN004@users.noreply.github.com>
2024-02-12 15:05:58 -0500

    Update wiper.cpp

--- wiper.cpp
+++ wiper.cpp
@@ -4,3 +4,4 @@
 #include "arm_book_lib.h"
 #include "wiper.h"
 //=====[Declaration of private defines]========================================
+//=====[Declaration of private data types]=====================================
