38649416ebb2f61c70c92e80c4cbb53e79c6f920
PAPAN004 <155662184+PAPAN004@users.noreply.github.com>
2024-02-12 15:07:18 -0500

    Update wiper.cpp

--- wiper.cpp
+++ wiper.cpp
@@ -4,13 +4,19 @@
 #include "arm_book_lib.h"
 #include "wiper.h"
 //=====[Declaration of private defines]========================================
+
 //=====[Declaration of private data types]=====================================
+
 //=====[Declaration and initialization of public global objects]===============
+
 //=====[Declaration of external public global variables]=======================
 
 //=====[Declaration and initialization of public global variables]=============
 
 //=====[Declaration and initialization of private global variables]============
+
 //=====[Declarations (prototypes) of private functions]========================
 
 //=====[Implementations of public functions]===================================
+
+//=====[Implementations of private functions]==================================
