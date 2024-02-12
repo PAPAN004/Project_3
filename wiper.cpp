824be049a7a826d0a76a1a2cf036543ffa8b3689
PAPAN004 <155662184+PAPAN004@users.noreply.github.com>
2024-02-12 15:11:51 -0500

    Update wiper.cpp

--- wiper.cpp
+++ wiper.cpp
@@ -4,7 +4,13 @@
 #include "arm_book_lib.h"
 #include "wiper.h"
 //=====[Declaration of private defines]========================================
-
+#define W_OFF_TH   0.25
+#define W_LO_TH  0.50
+#define W_HIGH_TH   0.75 
+#define INT_SHORT_TH  0.3
+#define INT_MEDIUM_TH   0.7 
+#define DUTY_MIN 0.017
+#define DUTY_MAX 0.129
 //=====[Declaration of private data types]=====================================
 
 //=====[Declaration and initialization of public global objects]===============
