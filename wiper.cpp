0f12ea1db490cf018d4cfa447283d082fd013577
PAPAN004 <155662184+PAPAN004@users.noreply.github.com>
2024-02-12 15:06:30 -0500

    Update wiper.cpp

--- wiper.cpp
+++ wiper.cpp
@@ -6,3 +6,8 @@
 //=====[Declaration of private defines]========================================
 //=====[Declaration of private data types]=====================================
 //=====[Declaration and initialization of public global objects]===============
+//=====[Declaration of external public global variables]=======================
+
+//=====[Declaration and initialization of public global variables]=============
+
+//=====[Declaration and initialization of private global variables]============
