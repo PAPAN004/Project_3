2ded8fc6adb7da2172375e10a6d02a231c2324c4
PAPAN004 <155662184+PAPAN004@users.noreply.github.com>
2024-02-12 15:12:04 -0500

    Update wiper.h

--- wiper.h
+++ wiper.h
@@ -5,7 +5,21 @@
 //=====[Declaration of public defines]=========================================
 
 //=====[Declaration of public data types]======================================
+typedef enum wMode {
+    W_HIGH,
+    W_LO,
+    W_INT,
+    W_OFF,
+} wMode_t;
 
+typedef enum intMode {
+    INT_SHORT,
+    INT_MEDIUM,
+    INT_LONG,
+} intMode_t;
+
+wMode_t wiperMode;
+intMode_t intervalMode;
 //=====[Declarations (prototypes) of public functions]=========================
 
 //=====[#include guards - end]=================================================
