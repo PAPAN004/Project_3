5e4b09bb1d8b9a4c01e66a6bd9d1adfd87408bde
PAPAN004 <155662184+PAPAN004@users.noreply.github.com>
2024-02-14 15:55:45 -0500

    Update user_interface.h

--- user_interface.h
+++ user_interface.h
@@ -1,1 +1,14 @@
+//=====[#include guards - begin]===============================================
 
+#ifndef _USER_INTERFACE_H_
+#define _USER_INTERFACE_H_
+
+//=====[Declaration of public defines]=========================================
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+//=====[#include guards - end]=================================================
+
+#endif // _USER_INTERFACE_H_
