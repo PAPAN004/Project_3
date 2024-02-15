146969b38dc56d3bbb873e5c67886e3fc3180716
mhelfri <155662290+mhelfri@users.noreply.github.com>
2024-02-14 19:17:53 -0500

    added servo functions and changed potentiometor settings code

--- wiper.cpp
+++ wiper.cpp
@@ -10,8 +10,12 @@
 #define W_HIGH_TH   0.75 
 #define INT_SHORT_TH  0.3
 #define INT_MEDIUM_TH   0.7 
-#define DUTY_MIN 0.021
-#define DUTY_MAX 0.055
+
+#define PERIOD_SEC              0.02
+#define DUTY_MIN                0.021
+#define DUTY_MAX                0.059
+
+#define SPEED_INCREMENT         0.001
 
 //=====[Declaration of private data types]=====================================
 
@@ -32,43 +36,64 @@ PwmOut wiper(PF_9);
 
 //=====[Implementations of public functions]===================================
 
-void wModeUpdate() {
-    float f = modePotentiometer.read();
-    if (wiperMode == W_OFF) {
-        if (f > W_OFF_TH) {
-            wiperMode = W_LO;
-        }
-        return;
-    }
-    if (wiperMode == W_LO) {
-        if (f < W_OFF_TH) {
-            wiperMode = W_OFF;
-        }
-        if (f > W_LO_TH) {
-            wiperMode = W_HIGH;
-        }
-        return;
-    }
-    if (wiperMode == W_HIGH) {
-        if (f < W_LO_TH) {
-            wiperMode = W_LO;
-        }
-        if (f > W_HIGH_TH) {
-            wiperMode = W_INT;
-        }
-        return;
-    if (wiperMode == W_INT) {
-        if (f < W_HIGH_TH) {
-            wiperMode = W_HIGH;
-        }
-        return;
-    }
-}
+void posServoInit()
+{
+    servo.period(PERIOD_SEC);
+    servo.write(DUTY_MIN);
 }
 
-void intModeUpdate() {
+static void ServoRiseUpdate(int speed)
+{
+    float pos_rise = 0.021;
+    
+    for (pos_rise=0.021; pos_rise<DUTY_MAX; pos_rise = pos_rise + SPEED_INCREMENT)
+    {
+        servo.write(pos_rise);
+        //replace this with incrementor
+        delay(speed);
+    }
+}
+
+static void ServoFallUpdate(int speed)
+{
+    float pos_fall = 0.059;
+    
+    for (pos_fall=0.059; pos_fall>DUTY_MIN; pos_fall = pos_fall - SPEED_INCREMENT)
+    {
+        servo.write(pos_fall);
+        //replace this with incrementor
+        delay(speed);
+    }
+
+}
+void wModeUpdate() {
+    float f = modePotentiometer.read();
+    // pot reading below 0.25, wipers set to off
+    if (f <= W_OFF_TH)
+    {   
+        wiperMode = W_OFF;
+    }
+    // pot reading is between 0.25 and 0.50, wipers set to low
+    else if (f > W_OFF_TH && f <= W_HIGH_TH)
+    {
+        wiperMode = W_LOW;
+    }
+    // pot reading is between 0.50 and 0.75, wipers set to high
+    else if (f > W_HIGH_TH && f <= W_INT_TH)
+    {
+        wiperMode = W_HIGH;
+    }
+    // pot reading is above 0.75, wipers set to interval
+    else if (f > W_INT_TH)
+    {
+        //wiperMode = W_INT;
+        intModeUpdate();
+    }
+}
+
+static void intModeUpdate() {
     float f = intPotentiometer.read();
-    if (wiperMode == W_INT) {
+    //if (wiperMode == W_INT) {
         if (intervalMode == INT_SHORT) {
             if (f > INT_SHORT_TH) {
                 intervalMode = INT_MEDIUM;
