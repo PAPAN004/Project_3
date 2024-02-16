a281fc7081dc389dc7050d7f48ab4b24d08aa592
PAPAN004 <155662184+PAPAN004@users.noreply.github.com>
2024-02-15 23:34:20 -0500

    Update ignition.h

--- ignition.h
+++ ignition.h
@@ -6,6 +6,15 @@
 
 //=====[Declaration of public data types]======================================
 
+typedef enum ButtonState {
+    B_ON,
+    B_RISING,
+    B_OFF,
+    B_FALLING,
+} ButtonState_t;
+
+ButtonState_t ignitionState;
+
 //=====[Declarations (prototypes) of public functions]=========================
 
 void ignitionInit();
