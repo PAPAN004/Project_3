ce4ce6491cd853692a52787f530e9a10578fa658
mhelfri <155662290+mhelfri@users.noreply.github.com>
2024-02-18 16:53:58 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 //=====[Libraries]=============================================================
 #include "mbed.h"
 #include "arm_book_lib.h"
+#inlcude "wiper.h"
+#include "display.h"
+#include "ignition.h"
+#include "user_interface.h"
 
 //=====[Main function, the program entry point after power on or reset]========
 
