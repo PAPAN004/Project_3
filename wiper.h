3aade489ae6377e072ea038b372dd4b0c7fdfa37
PAPAN004 <155662184+PAPAN004@users.noreply.github.com>
2024-02-19 23:20:27 -0500

    Update wiper.h

--- wiper.h
+++ wiper.h
@@ -7,7 +7,7 @@
 
 //=====[Declaration of public data types]======================================
 
-// this enumarated data type is used to tell the display module what state the wMode potentiometer
+// this enumarated data type is used to tell the system what mode the wiper-mode is in
 typedef enum wMode {
     W_HIGH,
     W_LO,
@@ -15,19 +15,20 @@ typedef enum wMode {
     W_OFF,
 } wMode_t;
 
-// this enumarated data type is used to tell the display module what state the intMode potentiometer
+// this enumarated data type is used to tell the system what mode the intermittent-mode in
 typedef enum intMode {
     INT_SHORT,
     INT_MEDIUM,
     INT_LONG,
 } intMode_t;
-
+// this enumarated data type is used to tell the system what point the servo motor is at
 typedef enum sMode{
     S_RISE,
     S_FALL,
     S_HOLD,
 } sMode_t;
 
+//alternate names for each enumerated data type to increase the readability of all code used
 wMode_t wiperMode;
 intMode_t intervalMode;
 sMode_t servoInstruction;
